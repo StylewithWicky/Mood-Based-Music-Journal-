e23266ea72ec5af99881e61193108b37a3083f7f
Yazz <itsjustmaitai@gmail.com>
2025-06-27 11:48:07 +0300

    Updated the moodlog to satisify the relationship

--- Backend/models/mood.py
+++ Backend/models/mood.py
@@ -8,3 +8,17 @@ class MoodLog(db.Model):
     mood=db.Column(db.String , nullable=False)
     journal_entry=db.Column(db.Text)
     created_at=db.Column(db.DateTime ,default=datetime.utcnow)
+
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    emotion_type_id = db.Column(db.Integer, db.ForeignKey('emotion_types.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "mood": self.mood,
+            "journal_entry": self.journal_entry,
+            "created_at": self.created_at.isoformat(),
+            "user": self.user.to_dict() if self.user else None,
+            "emotion_type": self.emotion_type.to_dict() if self.emotion_type else None
+        }
