02dda2919bf9adf43676ceb1fdc92e482809f992
Yazz <itsjustmaitai@gmail.com>
2025-06-27 12:20:51 +0300

    registered the routes in app.py

--- Backend/app.py
+++ Backend/app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from config import Config
 from models import db
 from controllers.mood_controller import mood_bp
+from controllers.emotional_controller import emotion_bp
+from controllers.user_controller import user_bp
 
 app=Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +16,8 @@ with app.app_context():
     db.create_all()
 
 app.register_blueprint(mood_bp)
+app.register_blueprint(user_bp)
+app.register_blueprint(emotion_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
